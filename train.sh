f7b624a24b7631d0fed54991c83e3af52c4d6dde
Jayant Sharma <jayantsharma1990@gmail.com>
2019-10-11 16:34:17 -0500

    minor

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 python -u train.py \
-  --max_epoch 20 \
+  --max_epoch 100 \
   --batch_size 32 \
   --model model_upconv \
-  --gpu 0 \
+  --gpu 1 \
   --learning_rate=1e-4 \
-  --epochs_to_wait=10 \
+  --epochs_to_wait=100 \
   --surface_loss_wt=1e-1 \
-  --log_dir log_bn_exp
+  --log_dir log_bn
